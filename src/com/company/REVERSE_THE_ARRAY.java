989d37b4b8172c99edc711d68d6dacd8d6c3a812
Rajesh Ghosh <110840094+ghoshraj@users.noreply.github.com>
2022-08-10 16:45:08 +0530

    Initial commit

---
+++ src/com/company/REVERSE_THE_ARRAY.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class REVERSE_THE_ARRAY {
+    public static void main(String[] args) {
+        int []n = new int []{10 ,20 ,30 ,40 ,50};
+        for(int i = n.length-1;i>= 0;i--){
+            System.out.println(n[i]);
+       }
+    }
+}
